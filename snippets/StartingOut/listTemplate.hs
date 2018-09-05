7750ee5010dfb46c64d89c5d0aa2185a53d1b808
Utkarsh Gupta <guptautkarsh2102@gmail.com>
2018-09-05 06:01:06 +0530

    Add listTemplate.hs
    Signed-off-by: Utkarsh Gupta <guptautkarsh2102@gmail.com>

---
+++ snippets/StartingOut/listTemplate.hs
@@ -0,0 +1,27 @@
+a = [1,2,3,4,5]
+b = 0 : a
+c = ['a','b','c','d','e']
+d = []
+e = a ++ [6,7,8,9,0]
+f = a !! 3
+g = [a,e]
+h = a>e
+i = head a
+j = tail a 
+k = last a 
+l = init a 
+m = length a 
+n = null a 
+o = reverse a 
+p = take 3 a 
+q = drop 3 a 
+r = reverse (drop 3 (reverse a))
+s = maximum a 
+t = minimum a 
+u = sum a
+v = product a 
+w = 3 `elem` a 
+--x
+--y
+--z
+
